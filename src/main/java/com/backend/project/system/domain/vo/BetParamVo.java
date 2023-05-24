8cd7673581d8edc6836ad3db5adc49d18b0d4dd3
eric909520 <eric909520@gmail.com>
2023-05-24 15:48:42 +0800

    bet param

---
+++ src/main/java/com/backend/project/system/domain/vo/BetParamVo.java
@@ -0,0 +1,45 @@
+package com.backend.project.system.domain.vo;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * 投注参数信息vo类
+ */
+@Data
+public class BetParamVo implements Serializable {
+
+    private Double betAmountZero;
+
+    private Double oddsZero;
+
+    private Double reward0;
+
+    private Double betAmountOne;
+
+    private Double oddsOne;
+
+    private Double reward1;
+
+    private Double betAmountTwo;
+
+    private Double oddsTwo;
+
+    private Double reward2;
+
+    private Double betAmountThree;
+
+    private Double oddsThree;
+
+    private Double reward3;
+
+    private Double betAmountHg;
+
+    private Double oddsHg;
+
+    private Double rewardHG3;
+
+    private Double rewardHGOther;
+
+}
