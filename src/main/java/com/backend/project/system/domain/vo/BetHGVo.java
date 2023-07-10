08809cc0b522a7e2c23209b79fadde63db45b44c
franktongs <francis.tongs@gmail.com>
2023-07-10 21:02:54 +0800

    test

--- src/main/java/com/backend/project/system/domain/vo/BetHGVo.java
+++ src/main/java/com/backend/project/system/domain/vo/BetHGVo.java
@@ -14,108 +14,108 @@ public class BetHGVo implements Serializable {
      * 体彩小，皇冠大
      */
     // 大1.5赔率
-    private Double 大15;
+    private Double 大15 = 0D;
 
-    private Double 大15Amount;
+    private Double 大15Amount = 0D;
 
     // 大2赔率
-    private Double 大2;
+    private Double 大2 = 0D;
 
-    private Double 大2Amount;
+    private Double 大2Amount = 0D;
 
     // 大2.5赔率
-    private Double 大25;
+    private Double 大25 = 0D;
 
-    private Double 大25Amount;
+    private Double 大25Amount = 0D;
 
     // 大3赔率
-    private Double 大3;
+    private Double 大3 = 0D;
 
-    private Double 大3Amount;
+    private Double 大3Amount = 0D;
 
     // 大3.5赔率
-    private Double 大35;
+    private Double 大35 = 0D;
 
-    private Double 大35Amount;
+    private Double 大35Amount = 0D;
 
     // 大1.5/2赔率
-    private Double 大15_2;
+    private Double 大15_2 = 0D;
 
-    private Double 大15_2Amount;
+    private Double 大15_2Amount = 0D;
 
     // 大2/2.5赔率
-    private Double 大2_25;
+    private Double 大2_25 = 0D;
 
-    private Double 大2_25Amount;
+    private Double 大2_25Amount = 0D;
 
     // 大2.5/3赔率
-    private Double 大25_3;
+    private Double 大25_3 = 0D;
 
-    private Double 大25_3Amount;
+    private Double 大25_3Amount = 0D;
 
     // 大3/3.5赔率
-    private Double 大3_35;
+    private Double 大3_35 = 0D;
 
-    private Double 大3_35Amount;
+    private Double 大3_35Amount = 0D;
 
     /**
      * 体彩大，皇冠小
      */
     // 小2.5赔率
-    private Double 小25;
+    private Double 小25 = 0D;
 
-    private Double 小25Amount;
+    private Double 小25Amount = 0D;
 
     // 小3.5赔率
-    private Double 小35;
+    private Double 小35 = 0D;
 
-    private Double 小35Amount;
+    private Double 小35Amount = 0D;
 
     // 小2/2.5赔率
-    private Double 小2_25;
+    private Double 小2_25 = 0D;
 
-    private Double 小2_25Amount;
+    private Double 小2_25Amount = 0D;
 
     // 小2.5/3赔率
-    private Double 小25_3;
+    private Double 小25_3 = 0D;
 
-    private Double 小25_3Amount;
+    private Double 小25_3Amount = 0D;
 
     // 小3/3.5赔率
-    private Double 小3_35;
+    private Double 小3_35 = 0D;
 
-    private Double 小3_35Amount;
+    private Double 小3_35Amount = 0D;
 
     // 小3.5/4赔率
-    private Double 小35_4;
+    private Double 小35_4 = 0D;
 
-    private Double 小35_4Amount;
+    private Double 小35_4Amount = 0D;
 
     // 总进球 0-1
-    private Double zong0_1;
+    private Double zong0_1 = 0D;
 
-    private Double zong0_1Amount;
+    private Double zong0_1Amount = 0D;
 
     private Double rewardZong0_1 = 0D;
 
     // 总进球 2-3
-    private Double zong2_3;
+    private Double zong2_3 = 0D;
 
-    private Double zong2_3Amount;
+    private Double zong2_3Amount = 0D;
 
     private Double rewardZong2_3 = 0D;
 
     // 总进球 4-6
-    private Double zong4_6;
+    private Double zong4_6 = 0D;
 
-    private Double zong4_6Amount;
+    private Double zong4_6Amount = 0D;
 
     private Double rewardZong4_6 = 0D;
 
     // 总进球 7+
-    private Double zong7;
+    private Double zong7 = 0D;
 
-    private Double zong7Amount;
+    private Double zong7Amount = 0D;
 
     private Double rewardZong7 = 0D;
 
@@ -124,38 +124,38 @@ public class BetHGVo implements Serializable {
      */
     /** 让球赔率 */
     // 主队加
-    private Double homeAdd05;
+    private Double homeAdd05 = 0D;
 
-    private Double homeAdd05Amount;
+    private Double homeAdd05Amount = 0D;
 
     // 主队减
-    private Double homeCut05;
+    private Double homeCut05 = 0D;
 
-    private Double homeCut05Amount;
+    private Double homeCut05Amount = 0D;
 
     // 客队加
-    private Double visitAdd05;
+    private Double visitAdd05 = 0D;
 
-    private Double visitAdd05Amount;
+    private Double visitAdd05Amount = 0D;
 
     // 客队减
-    private Double visitCut05;
+    private Double visitCut05 = 0D;
 
-    private Double visitCut05Amount;
+    private Double visitCut05Amount = 0D;
 
     // 主队胜
-    private Double home;
+    private Double home = 0D;
 
-    private Double homeAmount;
+    private Double homeAmount = 0D;
 
     // 和局
-    private Double tie;
+    private Double tie = 0D;
 
-    private Double tieAmount;
+    private Double tieAmount = 0D;
 
     // 客队胜
-    private Double visit;
+    private Double visit = 0D;
 
-    private Double visitAmount;
+    private Double visitAmount = 0D;
 
 }
--- src/main/java/com/backend/project/system/domain/vo/BetParamVo.java
+++ src/main/java/com/backend/project/system/domain/vo/BetParamVo.java
@@ -13,59 +13,59 @@ public class BetParamVo extends BetHGVo implements Serializable {
     /**
      * 投注金额基数
      */
-    private Double betBaseAmount;
+    private Double betBaseAmount = 0D;
 
-    private Double betAmountZero;
+    private Double betAmountZero = 0D;
 
     private Double oddsZero = 0d;
 
     private Double reward0 = 0D;
 
-    private Double betAmountOne;
+    private Double betAmountOne = 0D;
 
-    private Double oddsOne;
+    private Double oddsOne = 0D;
 
     private Double reward1 = 0D;
 
-    private Double betAmountTwo;
+    private Double betAmountTwo = 0D;
 
-    private Double oddsTwo;
+    private Double oddsTwo = 0D;
 
     private Double reward2 = 0D;
 
-    private Double betAmountThree;
+    private Double betAmountThree = 0D;
 
-    private Double oddsThree;
+    private Double oddsThree = 0D;
 
     private Double reward3 = 0D;
 
-    private Double betAmountFour;
+    private Double betAmountFour = 0D;
 
     private Double oddsFour = 0d;
 
     private Double reward4 = 0D;
 
-    private Double betAmountFive;
+    private Double betAmountFive = 0D;
 
-    private Double oddsFive;
+    private Double oddsFive = 0D;
 
     private Double reward5 = 0D;
 
-    private Double betAmountSix;
+    private Double betAmountSix = 0D;
 
-    private Double oddsSix;
+    private Double oddsSix = 0D;
 
     private Double reward6 = 0D;
 
-    private Double betAmountSeven;
+    private Double betAmountSeven = 0D;
 
-    private Double oddsSeven;
+    private Double oddsSeven = 0D;
 
     private Double reward7 = 0D;
 
-    private Double betAmountHg;
+    private Double betAmountHg = 0D;
 
-    private Double oddsHg;
+    private Double oddsHg = 0D;
 
     private Double rewardHG = 0D;
 
@@ -73,38 +73,43 @@ public class BetParamVo extends BetHGVo implements Serializable {
      * 单关参数 - 体彩
      */
     // 胜
-    private Double betAmountWin;
+    private Double betAmountWin = 0D;
 
-    private Double oddsWin;
+    private Double oddsWin = 0D;
 
     // 平
-    private Double betAmountTie;
+    private Double betAmountTie = 0D;
 
-    private Double oddsTie;
+    private Double oddsTie = 0D;
 
     // 负
-    private Double betAmountLose;
+    private Double betAmountLose = 0D;
 
-    private Double oddsLose;
+    private Double oddsLose = 0D;
 
     // 让胜 体彩：主队减 胜
-    private Double betAmountRangWin;
+    private Double betAmountRangWin = 0D;
 
-    private Double oddsRangWin;
+    private Double oddsRangWin = 0D;
 
     // 让负 体彩：主队减 负
-    private Double betAmountRangLose;
+    private Double betAmountRangLose = 0D;
 
-    private Double oddsRangLose;
+    private Double oddsRangLose = 0D;
 
     // 受让胜 体彩：主队加 胜
-    private Double betAmountShouWin;
+    private Double betAmountShouWin = 0D;
 
-    private Double oddsShouWin;
+    private Double oddsShouWin = 0D;
 
     // 受让负 体彩：主队加 负
-    private Double betAmountShouLose;
+    private Double betAmountShouLose = 0D;
 
-    private Double oddsShouLose;
+    private Double oddsShouLose = 0D;
+
+    /**
+     * 通知消息
+     */
+    private String msg;
 
 }
